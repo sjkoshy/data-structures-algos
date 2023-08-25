0bc3cf6e5c49ac5f9ea04ab2d53429bb2ad7e7ee
Sherilyn Koshy <sherilyn.koshy@gmail.com>
2023-08-25 13:52:26 -0400

    add graphs code

---
+++ graphs.js
@@ -0,0 +1,45 @@
+class Graph {
+	constructor() {
+		this.adjacencyList = {}
+
+	}
+
+	addVertex(vertex){
+		if (!this.adjacencyList[vertex]) {
+			this.adjacentList[vertex] = []
+    }
+	}
+	
+	addEdge(v1, v2){
+		this.adjacencyList[v1].push(v2)
+		this.adjacencyList[v2].push(v1)
+	}
+
+	removeEdge(v1, v2){
+		this.adjacencyList[v1] = this.adjacencyList[v1].filter((v)=> v!== v2)
+		this.adjacencyList[v2] = this.adjacencyList[v2].filter((v)=> v!==v1)
+	}
+
+	removeVertex(vertex){
+		while(this.adjacencyList[vertex].length){
+			const adjacentVertex = this.adjacencyList[vertex].pop()
+			this.removeEdge(vertex, adjacentVertex)
+   }
+
+		delete this.adjacencyList[vertex]
+ }
+}
+
+
+let g = new Graph()
+g.addVertex("Dallas")
+g.addVertex("Tokyo")
+g.addVertex("Aspen")
+g.addVertex("Los Angeles")
+g.addVertex("Hong Kong")
+g.addEdge("Dallas", "Tokyo")
+g.addEdge("Dallas", "Aspen")
+g.addEdge("Dallas", "Los Angeles")
+g.addEdge("Dallas", "Hong Kong")
+g.removeVertex("Aspen")
+console.log(g)
